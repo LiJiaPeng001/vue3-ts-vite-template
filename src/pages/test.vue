01c6f05fcc7b59c4a61a336a5c52d17390544562
lijiapeng001 <1146417576@qq.com>
2022-07-13 16:49:26 +0800

    fix: bug

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,3 +1,22 @@
 <template>
-  <div text-30 text-center font-600 m-100>test</div>
+  <div
+    ref="container"
+    w-500
+    h-500
+    overflow-y-auto
+    bg-pink-3
+    b-rounded-12
+    mx-auto
+    my-20
+  >
+    <div h-200 bg-lime-1></div>
+    <div h-200 bg-lime-2></div>
+    <div h-200 bg-lime-3></div>
+    <div h-200 bg-lime-4></div>
+    <div h-200 bg-lime-5></div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+// console.log(useLoadMore, "more");
+</script>
---
+++ src/utils/loadmore.ts
@@ -0,0 +1,78 @@
+import { throttle } from "./throttle-debounce";
+
+/**
+ * @param {Function} - toLoadMore 底部触发事件
+ * @param {Function} - actionFn   滚动触发事件
+ * @param {Number} - time 节流时间间隔
+ * @param {Element} ele 滚动Element 默认body
+ */
+let noop = () => { }
+
+interface PropsOptions {
+  toLoadMore?: Function,
+  actionFn?: Function,
+  time?: number
+  ele?: Element
+}
+
+interface RectOptions {
+  clientHeight: number,
+  scrollTop: number,
+  scrollHeight: number
+}
+
+class MoreMain {
+  public loadMoreFn: EventListener
+  public moreOptions: PropsOptions
+  constructor(props: PropsOptions) {
+    this.moreOptions = props
+    if (!this.moreOptions.ele) this.moreOptions.ele = document.body || document.documentElement
+    this.loadMoreFn = noop
+  }
+  // 获取三围
+  getElementClientRect(): RectOptions {
+    let ele = this.moreOptions.ele as Element
+    return {
+      clientHeight: ele.clientHeight,
+      scrollTop: ele.scrollTop,
+      scrollHeight: ele.scrollHeight
+    }
+  }
+  // 开启监听
+  mounted() {
+    let ele = this.moreOptions.ele as Element
+    ele.addEventListener("scroll", this.loadMoreFn, { passive: false });
+  }
+  // 取消监听
+  destroy() {
+    let ele = this.moreOptions.ele as Element
+    if (this.loadMoreFn) {
+      ele.removeEventListener("scroll", this.loadMoreFn);
+      this.loadMoreFn = noop;
+    }
+  }
+}
+
+export default class LoadMore extends MoreMain {
+  constructor(props: PropsOptions) {
+    super(props)
+    let { time = 300, toLoadMore = noop, actionFn = noop } = props
+    this.loadMoreFn = throttle(() => {
+      let { scrollTop, clientHeight, scrollHeight } = this.getElementClientRect()
+      actionFn(scrollTop);
+      if (scrollTop + clientHeight >= scrollHeight - 80) {
+        toLoadMore();
+      }
+    }, time)
+  }
+}
+
+export function useLoadMore(props: PropsOptions) {
+  let loadmore = new LoadMore(props)
+  onMounted(() => {
+    loadmore.mounted()
+  })
+  onUnmounted(() => {
+    loadmore.destroy()
+  })
+}
